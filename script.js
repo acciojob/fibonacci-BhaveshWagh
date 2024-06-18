6104d178eb5285484c5e6a6dad26096c475ad695
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-18 20:47:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
 // your code here
+	    if (num < 0 || num > 50) {
+        throw new Error("Input should be between 0 and 50 inclusive.");
+    }
+
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return a;
 }
 
 module.exports = fibonacci;
