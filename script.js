61662f134a22ef3f4288f8fe6162958fbbef8f56
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-25 16:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function fibonacci(num) {
     // }
 
     if (num === 0) return 0;
-    if (num === 1) return 1;
+    if (num === 1) return 0;
 
     let a = 0, b = 1;
     for (let i = 2; i <= num; i++) {
